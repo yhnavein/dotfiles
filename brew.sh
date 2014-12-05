6ad46ca7004e97322dbc0614c3182ff66a5a2b60
Piotrek Dąbrowski <floubadour@gmail.com>
2014-12-05 10:49:53 +0000

    added configuration for OSX and brew

---
+++ brew.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Ask for the administrator password upfront.
+sudo -v
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Install GNU core utilities (those that come with OS X are outdated).
+brew install coreutils
+
+# Install some other useful utilities like `sponge`.
+brew install moreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+
+brew install htop tmux vim
+
+brew install wget zsh git mercurial tig
+
+brew install node
+
+brew install mysql postgresql
+
+#Java stuff
+
+brew install maven
+
+
+
+
+# Remove outdated versions from the cellar.
+brew cleanup
