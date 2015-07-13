b3db64c89d2753bd777fe47b4c0464cf2170c5ca
Piotrek Dąbrowski <floubadour@gmail.com>
2015-07-14 00:04:47 +0200

    add eslint to npm

--- npm.sh
+++ npm.sh
@@ -3,6 +3,6 @@
 # Ask for the administrator password upfront.
 sudo -v
 
-npm install -g bower jshint grunt-cli karma-cli nodemon node-inspector node-static
+npm install -g bower jshint eslint grunt-cli karma-cli nodemon node-inspector node-static
 
 npm install -g git-stats npm-check-updates git-stats-importer
