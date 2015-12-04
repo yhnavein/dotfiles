e84a64866efc2ad5cc0526802d6889994c70e9ee
Piotrek Dąbrowski <floubadour@gmail.com>
2015-12-04 11:13:43 +0100

    added script for Windows Enterprise in corporate
    makes life easier

---
+++ windows-enterprise.ps1
@@ -0,0 +1,11 @@
+# Blank page as a Start Page
+Set-ItemProperty "HKCU:\Software\Microsoft\Internet Explorer\Main" "Start Page" "about:blank"
+
+# Turn off Automatic Configuration Script
+$registryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings"
+New-ItemProperty -Path $registryPath -Name "AutoDetect" -Value "0" -PropertyType DWORD -Force | Out-Null
+
+Remove-ItemProperty -Name "AutoConfigURL" -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings"
+
+# Turn off UAC
+Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" "EnableLUA" "0"
