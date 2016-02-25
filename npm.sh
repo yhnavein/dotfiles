473bda5ef5e413ef3376e4ca5d1d3e2357113e5c
Piotrek Dąbrowski <floubadour@gmail.com>
2016-02-25 22:23:39 +0100

    added gulp to the npm.sh

--- npm.sh
+++ npm.sh
@@ -3,7 +3,7 @@
 # Ask for the administrator password upfront.
 sudo -v
 
-npm install -g bower jshint eslint grunt-cli karma-cli nodemon node-inspector node-static
+npm install -g bower  eslint grunt-cli gulp-cli karma-cli nodemon node-inspector node-static
 
 npm install -g git-stats npm-check git-stats-importer
 
