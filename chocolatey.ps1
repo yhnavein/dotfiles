435748a49263ed40538c21e801f5ed1dc9d378a2
Piotr Dąbrowski <floubadour@gmail.com>
2021-12-10 14:35:29 -0800

    chore: upgrade install scripts

--- chocolatey.ps1
+++ chocolatey.ps1
@@ -1,52 +1,50 @@
 # Custom version of https://github.com/jayharris/dotfiles-windows
 
-if ((which cinst) -eq $null) {
+if ((which choco) -eq $null) {
     iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
     $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 }
 
-cinst curl
-#cinst dropbox
-#cinst evernote
-cinst notion
-# cinst filezilla
-# cinst githubforwindows
-cinst git.install
-cinst nodejs.install
-# cinst googlechrome
-# cinst googlechrome.canary
-# cinst hg
-# cinst fiddler
-cinst postman
-# cinst firefox # Sorry Firefox, Proton is disgusting
-cinst nuget.commandline
-cinst opera
-cinst vivaldi
-#cinst putty
-#cinst ruby
-# cinst skype
-cinst sublimetext3
-#cinst virtualclonedrive
-cinst vlc
-# cinst webpi
-# cinst webpicommandline
-# cinst linqpad
-cinst wget
-cinst wput
-# cinst wincommandpaste
-# cinst winmerge
+choco vscode
+choco telegram
+choco curl
+choco notion
+choco bandizip
+# choco filezilla
+# choco githubforwindows
+choco git.install
+choco nodejs.install
+choco golang
+choco deno
+choco googlechrome
+choco postman
+# choco firefox # Sorry Firefox, Proton is disgusting
+choco nuget.commandline
+choco opera
+choco vivaldi
+choco sublimetext4
+#choco virtualclonedrive
+choco vlc
+choco wget
+choco wput
+# choco wincommandpaste
+# choco winmerge
 
-cinst paint.net
-# cinst virtualbox
-cinst calibre
-cinst winscp
-cinst kdiff3
-# cinst spotify
-cinst notepad2
-cinst veracrypt
-# cinst lastpass
-cinst pdfxchangeviewer
-cinst picotorrent
-# cinst transmission-qt
-cinst aimp
-# cinst krita
+choco docker-desktop
+choco paint.net
+# choco virtualbox
+choco calibre
+choco winscp
+choco kdiff3
+choco notepad2
+choco veracrypt
+choco pdfxchangeviewer
+choco picotorrent
+# choco transmission-qt
+choco aimp
+# choco krita
+choco speedcrunch
+choco git-fork
+choco slack
+choco discord
+choco sharex
--- npm.sh
+++ npm.sh
@@ -4,13 +4,10 @@
 sudo -v
 
 # Basic stuff
-npm install --unsafe-perm -g eslint nodemon node-inspector node-static npm-check typescript prettier
+npm install --unsafe-perm -g eslint nodemon node-static npm-check typescript prettier dprint
 
 # 3rd party stuff
-npm install --unsafe-perm -g mjml netlify-cli now redoc-cli madge source-map-explorer
-
-# OpenAPI tools
-npm install --unsafe-perm -g swaggie restful-react
+npm install --unsafe-perm -g madge source-map-explorer vercel netlify-cli
 
 # FrontEnd Framework used everyday
-npm install --unsafe-perm -g create-react-app @vue/cli expo-cli @nestjs/cli
+npm install --unsafe-perm -g @vue/cli expo-cli @nestjs/cli sucrase
